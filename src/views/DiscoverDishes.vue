dca781ee1fac7b33f8d38374c14326f63352ee5d
Andrew Drury <drurya99@gmail.com>
2019-02-24 10:46:01 -0500

    discover dishes styling

--- src/views/DiscoverDishes.vue
+++ src/views/DiscoverDishes.vue
@@ -1,26 +1,55 @@
 <template>
   <div class="discover-dishes verticalCenter">
     <b-container fluid class="p-0">
-      <b-col class="p-0">
-        <b-row class="card-top m-0">
-          <b-col>
-            <b-row>
-              <div style="font-size: 20px;">Discover Dishes</div>
-            </b-row>
-            <b-row class="my-3">
-              <b-btn
-                class="btn btn-light"
-                style="font-size: 11px; width: 14em; height: 2em; padding-top: 2px; padding-bottom: 2px;"
-              >Search</b-btn>
-            </b-row>
-          </b-col>
-        </b-row>
+      <b-row class="card-top m-0">
+        <b-col>
+          <b-row>
+            <div style="font-size: 20px;">Discover Dishes</div>
+          </b-row>
+          <b-row class="my-3">
+            <b-btn
+              class="btn btn-light"
+              style="font-size: 11px; width: 14em; height: 2em; padding-top: 2px; padding-bottom: 2px;"
+            >Search</b-btn>
+          </b-row>
+        </b-col>
+      </b-row>
 
-        <b-card class="card-gen" style="margin: auto;">
+      <b-col>
+        <b-card class="card-gen mt-3" style="margin: auto;">
           <b-col>
             <b-row>
               <b-col>
-                <b-row class="image imageItem left"></b-row>
+                <b-row class="image imageItem"></b-row>
+              </b-col>
+            </b-row>
+            <b-row class="pt-2">
+              <b-col class="text-left pl-0 pt-1" style="font-size: 14px;">55 bites</b-col>
+              <div
+                class="float-right rightAlign"
+                style="background: url('/img/star.png'); background-size: cover;"
+              >
+                <h4>&nbsp;</h4>
+              </div>
+            </b-row>
+            <b-row class="text-left">
+              <label style="font-size: 13px;">
+                Posted by
+                <b>
+                  <u>
+                    <a to="/other-profile">Amy King</a>
+                  </u>
+                </b>
+                5 hours ago
+              </label>
+            </b-row>
+          </b-col>
+        </b-card>
+        <b-card class="card-gen mt-3" style="margin: auto;">
+          <b-col>
+            <b-row>
+              <b-col>
+                <b-row class="image imageItem"></b-row>
               </b-col>
             </b-row>
             <b-row class="pt-2">
@@ -89,13 +118,10 @@
 .imageItem {
   background: url("/img/pizza1.jpg");
   background-size: cover;
+  width: 71vw;
+  height: 20vh;
 }
 
-.left {
-  width: 90vw;
-  height: 171px;
-  margin-right: 0px;
-}
 // .full {
 //   width: 100%;
 //   height: 100%;
